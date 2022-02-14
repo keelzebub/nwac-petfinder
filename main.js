77acbbad3dca9e69dee2d64d481675f36322b2ec
Kellen Hawley <hawleykc@gmail.com>
2022-02-13 18:21:59 -0800

    update script to use createElement

--- main.js
+++ main.js
@@ -98,11 +98,8 @@ window.replaceIndex = async (animals) => {
 //
 // Replace the main pet image
 //
-window.replaceMainPetImage = async (url) => {
-  const mainImageContainer = document.getElementsByClassName('sqs-image-shape-container-element')[0];
-  const mainImage = `
-    <img class="thumb-image loaded" data-src="${url}" data-image="${url}" data-image-focal-point="0.5,0.5" alt="" data-load="false" data-type="image" src="${url}">
-  `;
+window.replaceMainPetImage = async (mainImage, imageUrl) => {
+  mainImage.src = imageUrl.replace(/600$/, '500');
 };
 
 //
@@ -117,6 +114,18 @@ window.replaceShow = async (animal) => {
     largeImages.push(photo.large);
   });
 
+  const mainContainer = document.getElementsByClassName('sqs-row')[0];
+
+  const leftContainer =  document.createElement('div');
+  leftContainer.className = 'col sqs-col-6 span-6';
+  leftContainer.style = 'padding: 0 17px';
+
+  const rightContainer = document.createElement('div');
+  rightContainer.className = 'col sqs-col-6 span-6';
+  rightContainer.style = 'padding: 0 17px 0 108px;';
+
+  mainContainer.appendChild(leftContainer);
+  mainContainer.appendChild(rightContainer);
 
   // Update the animal description
   let getsAlongWith = [];
@@ -127,41 +136,56 @@ window.replaceShow = async (animal) => {
   }
   getsAlongWith = getsAlongWith.length > 0 ? getsAlongWith.join(', ') : 'N/A';
 
+  const name = documet.createElement('h3');
+  name.className = 'preFade fadeIn';
+  name.innerText = animal.name;
+  rightContainer.appendChild(name);
 
-  const descriptionContainer = document.getElementsByClassName('sqs-block-content')[2];
-  const description = `
-<h3 style="white-space: pre-wrap; transition-timing-function: ease; transition-duration: 0.4s; transition-delay: 0.253333s;" class="preFade fadeIn">${animal.name}</h3>
-<p class="preFade fadeIn" style="white-space: pre-wrap; transition-timing-function: ease; transition-duration: 0.4s; transition-delay: 0.266667s;">
-  ${animal.age} ${animal.gender} ${animal.breeds.primary} ${animal.colors.primary}${animal.colors.secondary ? ', ' + animal.colors.secondary : ''}
-Coat length: ${animal.coat}
-House-trained: ${animal.attributes.house_trained ? 'Yes' : 'No'}
-Vaccinations up to date: ${animal.attributes.shots_current ? 'Yes' : 'No'}
-Spayed / neutered: ${animal.attributes.spayed_neutered ? 'Yes' : 'No'}
-Good in a home with: ${getsAlongWith}
-</p>
-<p class="sqsrte-small preFade fadeIn" style="white-space: pre-wrap; transition-timing-function: ease; transition-duration: 0.4s; transition-delay: 0.28s;">
-${animal.description}
-For full description, please visit the <a target='_blank' rel='noopener noreferrer' href='${animal.url}'>Petfinder page for ${animal.name}</a>!
-</p>
-  `;
+  const detailsContent = [
+    `${animal.age} ${animal.gender} ${animal.breeds.primary}`,
+    `Color: ${animal.colors.primary}${animal.colors.secondary ? ', ' + animal.colors.secondary : ''}`,
+    `Coat length: ${animal.coat}`,
+    `House-trained: ${animal.attributes.house_trained ? 'Yes' : 'No'}`,
+    `Vaccinations up to date: ${animal.attributes.shots_current ? 'Yes' : 'No'}`,
+    `Spayed / neutered: ${animal.attributes.spayed_neutered ? 'Yes' : 'No'}`,
+    `Good in a home with: ${getsAlongWith}`
+  ];
 
-  descriptionContainer.innerHTML = description;
+  const details = document.createElement('p');
+  details.className = 'preFade fadeIn';
+  details.style = 'white-space: pre-wrap; transition-timing-function: ease; transition-duration: 0.4s; transition-delay: 0.266667s;'
+  details.innerText = detailsContent.join("\n");
+  rightContainer.appendChild(details);
+
+  const description = document.createElement('p');
+  description.className = 'preFade fadeIn';
+  description.style = 'white-space: pre-wrap; transition-timing-function: ease; transition-duration: 0.4s; transition-delay: 0.266667s;'
+  description.innerText = `${animal.description}`
+  rightContainer.appendChild(description);
+
+  const forMoreInfo = document.createElement('p');
+  forMoreInfo.className = 'preFade fadeIn';
+  forMoreInfo.style = 'white-space: pre-wrap; transition-timing-function: ease; transition-duration: 0.4s; transition-delay: 0.266667s;'
+  forMoreInfo.innerHTML = `For full description, please visit the <a target='_blank' rel='noopener noreferrer' href='${animal.url}'>Petfinder page for ${animal.name}</a>!`
+  rightContainer.appendChild(forMoreInfo);
 
 
   // Update the images
-  window.replaceMainPetImage(largeImages[0]);
-
-  const imageContainer = document.getElementsByClassName('sqs-block-content')[0];
+  const mainImage = document.createElement('img');
+  mainImage.src = largeImages[0].replace(/600$/, '500');
+  mainImage.dataset['image-focal-point'] = '0.5,0.5';
+  mainImage.alt = `Primary image of ${animal.name}`;
+  leftContainer.appendChild(mainImage);
 
   const imageGallery = document.createElement('div');
   imageGallery.style = 'margin-top: 12px;';
 
-  imageContainer.appendChild(imageGallery);
+  leftContainer.appendChild(imageGallery);
 
-  const imageThumbnails = smallImages.forEach((url, index) => {
+  smallImages.forEach((url, index) => {
     const thumbnailButton = document.createElement('button');
     thumbnailButton.style = 'background: transparent; border: none;';
-    thumbnailButton.onClick = window.replaceMainPetImage(largeImages[index]);
+    thumbnailButton.onClick = window.replaceMainPetImage(mainImage, largeImages[index]);
     thumbnailButton.innerHTML = `<img data-id='${index}' src="${url}">`;
 
     imageGallery.appendChild(thumbnailButton);
